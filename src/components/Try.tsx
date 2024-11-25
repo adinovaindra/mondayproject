93188c307e8a5dbafbd6a11a197cbfe5935f9b4c
adinovaindra <adinovapramono@yahoo.co.id>
2024-11-25 10:20:25 +0700

    bismillah

---
+++ src/components/Try.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Color, HStack } from "@chakra-ui/react"
+
+const Try = () => {
+  return (
+    <HStack>
+      <Button bgColor={'whitesmoke'} >Click me</Button>
+      <Button bgColor={'whitesmoke'} >Click me</Button>
+    </HStack>
+  )
+}
+
+export default Try
